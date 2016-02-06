97dc7e8c32252e174e242b1b54b6a664cd5be204
jeekiii <iii.iii.iii.3x3@gmail.com>
2016-02-06 18:08:55 +0100

    fixed a bug that led to segfault in the ai

--- Objects/ai_tuto.cpp
+++ Objects/ai_tuto.cpp
@@ -8,10 +8,10 @@ AiTuto::AiTuto(Object *controlled)
 std::vector<CommandType> AiTuto::GiveCommands()
 {
 	std::vector<CommandType> result;
-	if(target_->getPosition().x < controlled_->getPosition().x)
+/*	if(target_->getPosition().x < controlled_->getPosition().x)
 		result.push_back(TURN_RIGHT_COMMAND);
 	else
-		result.push_back(TURN_LEFT_COMMAND);
+		result.push_back(TURN_LEFT_COMMAND);*/
 	result.push_back(ATTACK_COMMAND);
 	return result;
 }
